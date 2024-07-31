2e62d1a733a49c562561fa984d1a0fd4e2b6cc27
Wali Chizarum <155149253+realZarvin@users.noreply.github.com>
2024-07-31 13:51:45 +0100

    Create DC-main.rs

---
+++ src/DC-main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+
+async fn index() -> impl Responder {
+    "Hello, Rust with Actix-web!"
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
